d23b3f460a24f361563b852f8367e3e9c85b218e
ytd <ytd@vega.com.vn>
2018-12-17 18:14:20 +0700

    d

--- lib/ui/homeLive/home_live.dart
+++ lib/ui/homeLive/home_live.dart
@@ -11,16 +11,48 @@ class HomeLive extends StatelessWidget {
         child: new Column(
           children: <Widget>[
             new AudioLive(),
-            new Row(
-              children: <Widget>[
-                new Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    new Text('Cac lay long me chong(ep2)', style: TextStyle(color: Colors.white, fontSize: 16))
-                  ],
-                )
-              ],
-            )
+            new Row(children: <Widget>[
+              new Row(
+                children: <Widget>[
+                  new Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      new Text('Cac lay long me chong(ep2)', style: TextStyle(color: Colors.white, fontSize: 16)),
+                      new Row(
+                        children: <Widget>[
+                          new Icon(Icons.remove_red_eye),
+                          new Text('2.183 luot xem')
+                        ],
+                      ),
+                      new Row(
+                        children: <Widget>[
+                          new Icon(Icons.favorite),
+                          new Text('Yeu Thich')
+                        ],
+                      )
+                    ],
+                  )
+                ],
+              ),
+              new Row(
+                children: <Widget>[
+                  new Text('Tra loi cau hoi cua chuong trinh \n tich sao may man \n Nhan giai thuong hap dan'),
+                  new Container(
+                    decoration: new BoxDecoration(
+                      color: Colors.yellowAccent,
+                      borderRadius: const BorderRadius.all(const Radius.circular(15.0)),
+                    ),
+                    child: new Row(
+                      children: <Widget>[
+                        new Icon(Icons.send, size: 10, color: Colors.white,),
+                        new Text('Tra Loi Cau Hoi')
+                      ]
+                    ),
+                  )
+                ],
+              )
+            ],)
+            
           ],
         ),
       )
