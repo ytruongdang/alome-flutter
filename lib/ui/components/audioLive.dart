d157f81c432da882c5cfd1ed60254241c4d452cb
ytd <ytd@vega.com.vn>
2018-12-17 18:00:34 +0700

    d

--- lib/ui/components/audioLive.dart
+++ lib/ui/components/audioLive.dart
@@ -6,18 +6,30 @@ class AudioLive extends StatelessWidget {
     return new Row(
       children: <Widget>[
         new Container(
-          color: Colors.red,
+          decoration: new BoxDecoration(
+            borderRadius: const BorderRadius.all(const Radius.circular(5)),
+            color: Colors.red,
+          ),
           margin: new EdgeInsets.only(left: 16, right: 16),
           padding: new EdgeInsets.only(left: 10, right: 10, top: 5, bottom: 5),
           child: new Text('Live', style: TextStyle(color: Colors.white, fontSize: 12.0),),
         ),
-        new Container(width: 10.0,),
-        new Row(
-          children: <Widget>[
-            new Icon(Icons.panorama_fish_eye),
-            new Text(value, style: TextStyle(color: Colors.red))
-          ],
+        new Container(
+          padding: new EdgeInsets.only(left: 10, top: 5, right: 10, bottom: 5),
+          decoration: new BoxDecoration(
+            border: new Border.all(width: 0.0, color: Colors.transparent),
+            borderRadius: const BorderRadius.all(const Radius.circular(5.0)),
+            color: new Color.fromRGBO(0, 0, 0, 0.5)
+          ),
+          child: new Row(
+            children: <Widget>[
+              new Icon(Icons.remove_red_eye, size: 10.0, color: Colors.white),
+              new Container(width: 5.0,),
+              new Text(value, style: TextStyle(color: Colors.white, fontSize: 12.0))
+            ],
+          ),
         )
+        
       ],
     );
   }
@@ -27,17 +39,17 @@ class AudioLive extends StatelessWidget {
       padding: EdgeInsets.only(left: 0, right: 0),
       child: new Stack(
         children: <Widget>[
-          new Image.network('https://www.gettyimages.ie/gi-resources/images/Homepage/Hero/UK/CMS_Creative_164657191_Kingfisher.jpg', fit: BoxFit.contain, alignment: Alignment(0, 0),),
+          new Image.network('https://www.gettyimages.ie/gi-resources/images/Homepage/Hero/UK/CMS_Creative_164657191_Kingfisher.jpg', fit: BoxFit.cover, alignment: Alignment(0, 0),height: 250.0,),
           new Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               new Container(height: 10,),
-              _viewer(value: 'hello'),
+              _viewer(value: '127 nguoi dang xem'),
             ],
           ),          
         ],
       ),
-      height: 210.0,
+      height: 250.0,
     );
   }
 }
--- lib/ui/homeLive/home_live.dart
+++ lib/ui/homeLive/home_live.dart
@@ -4,11 +4,27 @@ import '../components/audioLive.dart';
 class HomeLive extends StatelessWidget {
   Widget build(BuildContext context) {
     return new Scaffold(
-      body: new Column(
-        children: <Widget>[
-          new AudioLive()
-        ],
-      ),
+      body: new Container(
+        decoration: new BoxDecoration(
+          color: Colors.red
+        ),
+        child: new Column(
+          children: <Widget>[
+            new AudioLive(),
+            new Row(
+              children: <Widget>[
+                new Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    new Text('Cac lay long me chong(ep2)', style: TextStyle(color: Colors.white, fontSize: 16))
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
+      )
+      
     );
   }
 }
